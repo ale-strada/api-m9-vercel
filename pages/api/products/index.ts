1c6168500afda5e32bfd4504d02a7b3052099f65
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-10-12 15:43:35 -0300

    se agrega /search

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,23 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getOffsetAndLimitFromReq } from "lib/requests";
-import { productsIndex } from "lib/algolia";
+import methods from "micro-method-router";
 
-//esta funcion devuelve un objeto con los resultados de busqueda en algolia
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  const { offset, limit } = getOffsetAndLimitFromReq(req, 10, 10000);
-
-  const results = await productsIndex.search(req.query.search as string, {
-    offset: offset,
-    length: limit,
-  });
-
-  //objeto con resultados
-  res.send({
-    results: results.hits,
-    pagination: {
-      limit,
-      offset,
-      total: results.nbHits,
-    },
-  });
-}
+export default methods({
+  async get(req: NextApiRequest, res: NextApiResponse) {
+    res.status(200).send({ m: "PRODUCTOS" });
+  },
+});
