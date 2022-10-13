5f3cd3c47cd229f4ba04f0f52b854d2f4a861aff
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-10-13 08:29:37 -0300

    prueba de funciones de notificacion

--- lib/controllers/order controllers.ts
+++ lib/controllers/order controllers.ts
@@ -13,27 +13,26 @@ export async function processOrder(topic, id) {
       myOrder.data.status = "closed";
       myOrder.data.externalOrder = order;
       await myOrder.push();
-      console.log({
-        email: myOrder.data.externalOrder.payer.email,
-        objectID: myOrder.data.productId,
-      });
 
-      //   sendEmailComprador(myOrder.data.externalOrder.payer.email);
-      //   saveOrder(orderId, myOrder.data.productId);
-      //send email (compra exitosa, procesando envio)
-      //email interno (alguien compro algo)
+      sendEmailComprador(myOrder.data.externalOrder.payer.email);
+      saveOrder(orderId, myOrder.data.productId);
     }
   }
 }
 
 function sendEmailComprador(email) {
-  const msg = {
-    to: email,
-    from: "buscador.de.mascotas.app@gmail.com",
-    subject: "informacion de compra",
-    text: "Su pago fue realizado con éxito",
-  };
-  sendEmail(msg);
+  const cleanEmail = email.trim().toLocaleLowerCase();
+  if (cleanEmail) {
+    const msg = {
+      to: cleanEmail,
+      from: "buscador.de.mascotas.app@gmail.com",
+      subject: "informacion de compra",
+      text: "Su pago fue realizado con éxito",
+    };
+    sendEmail(msg);
+  } else {
+    console.error("no hay direccion de email");
+  }
 }
 
 async function saveOrder(orderId, objectID) {
@@ -42,3 +41,36 @@ async function saveOrder(orderId, objectID) {
     objectID: objectID,
   });
 }
+
+// {
+//     productId: 'recjioEHvExJ261FB',
+//     status: 'closed',
+//     userId: 'VgVv4uqmb4YRH6ijSyY7',
+//     aditionalInfo: { address: 'una direccion para envio', color: 'verde' },
+//     externalOrder: {
+//       id: 6134811307,
+//       status: 'closed',
+//       external_reference: 'FQMwUKAqM7oCFTBJ72ng',
+//       preference_id: '1211601181-37312129-7acc-4d4a-834d-74182bb23776',
+//       payments: [ [Object] ],
+//       shipments: [],
+//       payouts: [],
+//       collector: { id: 1211601181, email: '', nickname: 'TETE1383884' },
+//       marketplace: 'NONE',
+//       notification_url: 'https://api-m9-vercel-ige5.vercel.app/api/ipn/mercadopago',
+//       date_created: '2022-10-13T07:17:27.573-04:00',
+//       last_updated: '2022-10-13T07:17:28.306-04:00',
+//       sponsor_id: null,
+//       shipping_cost: 0,
+//       total_amount: 1190,
+//       site_id: 'MLA',
+//       paid_amount: 1190,
+//       refunded_amount: 0,
+//       payer: { id: 1211611438, email: '' },
+//       items: [ [Object] ],
+//       cancelled: false,
+//       additional_info: '',
+//       application_id: null,
+//       order_status: 'paid'
+//     }
+//   }
