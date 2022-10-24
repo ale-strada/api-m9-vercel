98a201a53bdc26f00796491d0303556029280f88
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-10-24 08:34:51 -0300

    probando sengrid

--- pages/api/auth/index.ts
+++ pages/api/auth/index.ts
@@ -1,31 +1,31 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { sendCode } from "lib/models/auth";
-//import { sendEmail } from "lib/sendgrid";
-import { sendEmail } from "lib/mailgun";
+import { sendEmail } from "lib/sendgrid";
+//import { sendEmail } from "lib/mailgun";
 import methods from "micro-method-router";
 
 export default methods({
   async post(req: NextApiRequest, res: NextApiResponse) {
     const authcode = await sendCode(req.body.email);
+    // para enviar el email con mailgun
+    // const email = authcode.data.email;
+    // const subject = "Codigo para ingresar";
+    // const content =
+    //   "Tu codigo de seguridad para iniciar sesion es: " +
+    //   authcode.data.code.toString();
 
-    const email = authcode.data.email;
-    const subject = "Codigo para ingresar";
-    const content =
-      "Tu codigo de seguridad para iniciar sesion es: " +
-      authcode.data.code.toString();
-
-    sendEmail(email, content, subject);
+    // sendEmail(email, content, subject);
 
     // para enviar el email con sendgrid
-    // const msg = {
-    //   to: authcode.data.email,
-    //   from: "buscador.de.mascotas.app@gmail.com", // Use the email address or domain you verified above
-    //   subject: "Codigo para ingresar",
-    //   text:
-    //     "Tu codigo de seguridad para iniciar sesion es: " +
-    //     authcode.data.code.toString(),
-    // };
-    // sendEmail(msg);
+    const msg = {
+      to: authcode.data.email,
+      from: "strada.ale92@gmail.com", // Use the email address or domain you verified above
+      subject: "Codigo para ingresar",
+      text:
+        "Tu codigo de seguridad para iniciar sesion es: " +
+        authcode.data.code.toString(),
+    };
+    sendEmail(msg);
 
     res.send(authcode.data);
   },
