72bf2566d290b028fbbad0cec63c4a476aef305c
ale-strada <79310707+ale-strada@users.noreply.github.com>
2022-12-06 15:33:52 -0300

    update patch me phone 2

--- pages/api/me/index.ts
+++ pages/api/me/index.ts
@@ -12,7 +12,7 @@ let bodySchema = yup
     name: yup.string(),
     email: yup.string(),
     address: yup.string(),
-    phone: yup.number(),
+    phone: yup.string(),
   })
   .noUnknown()
   .strict(true);
